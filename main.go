94e9c704caeac524158f076fe64c0e9d19e3e50c
Philip <phillip.martin@gmail.com>
2016-02-29 07:25:53 +0000

    switch to ffjson for speed add various docstrings and comments tweak a lot of value vs ref passing for perf and correctness switch to ZeroCopy for packet capture switch to a NewDecodingLayerParser with more targeted layers pre-allocate a lot more persistant data structures handle TCP queries (although not multi-packet answers yet) add the ability to specify the number of packet handling threads add CPU profiling hooks extensive profiling and hotspot reduction

--- main.go
+++ main.go
@@ -7,22 +7,23 @@ import "strconv"
 import "time"
 import "net"
 import "os"
-import "encoding/json"
+import "bufio"
+import "runtime/pprof"
+import "encoding/binary"
 
-//import "github.com/Shopify/sarama"
 import "github.com/google/gopacket"
 import "github.com/google/gopacket/pcap"
 import "github.com/google/gopacket/layers"
-
+import "github.com/pquerna/ffjson/ffjson"
 /*
 Plans:
 
     -code cleanup (e.g. break up handlePacket, switch everything to camelCase)
-    stats output
-    perf testing
+	deal with DNS Length header in TCP    
+    -perf testing
 release v2
 
-	deal with DNS Length header in TCP
+	stats output
     re-build error handling with panic()/recover()
     syslog logging
     logging to kafka
@@ -35,6 +36,8 @@ release v3
 */
 
 /*
+
+Structs and helper functions
 
 DNS log entry struct and helper functions
 
@@ -55,44 +58,34 @@ type dnsLogEntry struct {
 	err     error  //encoding errors
 }
 
+//private, idempotent function that ensures the json is encoded
 func (dle *dnsLogEntry) ensureEncoded() {
 	if dle.encoded == nil && dle.err == nil {
-		dle.encoded, dle.err = json.Marshal(dle)
+		dle.encoded, dle.err = ffjson.Marshal(dle)
 	}
 }
 
+//returns length of the encoded JSON
 func (dle *dnsLogEntry) Length() int {
 	dle.ensureEncoded()
 	return len(dle.encoded)
 }
 
+//public method to encode the string
 func (dle *dnsLogEntry) Encode() ([]byte, error) {
 	dle.ensureEncoded()
 	return dle.encoded, dle.err
 }
 
+/*
+  struct for DNS connection table entry
+  the 'inserted' value is used in connection table cleanup
+*/
 type dnsMapEntry struct {
-	entry    *layers.DNS
+	entry    layers.DNS
 	inserted time.Time
 }
 
-//background task to clear out stale entries in the conntable
-//one of these gets spun up for every packet handling thread
-func cleanDnsCache(conntable *map[uint16]dnsMapEntry, maxAge time.Duration, interval time.Duration) {
-
-	for {
-		time.Sleep(interval)
-
-		//max_age should be negative, e.g. -1m
-		cleanupCutoff := time.Now().Add(maxAge)
-		for key, item := range *conntable {
-			if item.inserted.Before(cleanupCutoff) {
-				log.Debug("conntable GC: cleanup query ID " + strconv.Itoa(int(key)))
-				delete(*conntable, key)
-			}
-		}
-	}
-}
 
 /*
    The gopacket DNS layer doesn't have a lot of good String()
@@ -165,29 +158,12 @@ func RrString(rr layers.DNSResourceRecord) string {
 	}
 }
 
-func getIpaddrs(packet gopacket.Packet) (net.IP, net.IP) {
-	var srcIP net.IP = nil
-	var dstIP net.IP = nil
-
-	if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
-		ipData, _ := ipLayer.(*layers.IPv4)
-		srcIP = ipData.SrcIP
-		dstIP = ipData.DstIP
-	} else if ipLayer := packet.Layer(layers.LayerTypeIPv6); ipLayer != nil {
-		ipData, _ := ipLayer.(*layers.IPv6)
-		srcIP = ipData.SrcIP
-		dstIP = ipData.DstIP
-	} else {
-		//non-IP transport?  Ignore this packet
-		log.Debug("Got non-IP packet: " + packet.String())
-	}
-
-	return srcIP, dstIP
-}
-
-func initLogEntry(srcIP net.IP, dstIP net.IP, question *layers.DNS, reply *layers.DNS) []dnsLogEntry {
-	var retArray []dnsLogEntry
-
+/*
+	takes the src IP, dst IP, DNS question, DNS reply and the logs struct to populate.
+	returns nothing, but populates the logs array
+*/
+func initLogEntry(srcIP net.IP, dstIP net.IP, question layers.DNS, reply layers.DNS, logs *[]dnsLogEntry){
+	
 	/*
 	   http://forums.devshed.com/dns-36/dns-packet-question-section-1-a-183026.html
 	   multiple questions isn't really a thing, so we'll loop over the answers and
@@ -203,7 +179,7 @@ func initLogEntry(srcIP net.IP, dstIP net.IP, question *layers.DNS, reply *layer
 
 	if reply.ResponseCode != 0 {
 
-		tmpEntry := dnsLogEntry{
+		*logs = append(*logs, dnsLogEntry{
 			Query_ID:      reply.ID,
 			Question:      string(question.Questions[0].Name),
 			Response_Code: int(reply.ResponseCode),
@@ -216,15 +192,12 @@ func initLogEntry(srcIP net.IP, dstIP net.IP, question *layers.DNS, reply *layer
 			//...and goes to the client
 			Client:    dstIP,
 			Timestamp: time.Now().UTC().Format(time.RFC3339),
-		}
-		retArray = append(retArray, tmpEntry)
-
-		return retArray
+		})
 
 	} else {
 		for _, answer := range reply.Answers {
 
-			tmpEntry := dnsLogEntry{
+			*logs = append(*logs, dnsLogEntry{
 				Query_ID:      reply.ID,
 				Question:      string(question.Questions[0].Name),
 				Response_Code: int(reply.ResponseCode),
@@ -237,86 +210,116 @@ func initLogEntry(srcIP net.IP, dstIP net.IP, question *layers.DNS, reply *layer
 				//...and goes to the client
 				Client:    dstIP,
 				Timestamp: time.Now().UTC().Format(time.RFC3339),
-			}
-
-			retArray = append(retArray, tmpEntry)
+			})
 		}
-
-		return retArray
 	}
 }
 
-func getDnsLayer(packet gopacket.Packet) *layers.DNS {
+//background task to clear out stale entries in the conntable
+//one of these gets spun up for every packet handling thread
+//takes a pointer to the contable to clean, the maximum age of an entry and how often to run GC
+func cleanDnsCache(conntable *map[uint16]dnsMapEntry, maxAge time.Duration, interval time.Duration) {
 
-	if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
-		if dnsLayer := packet.Layer(layers.LayerTypeDNS); dnsLayer != nil {
-			return dnsLayer.(*layers.DNS)
-		} else {
-			log.Debug("Got a non-DNS UDP packet")
-			log.Debug(packet.String())
-			return nil
-		}
-	} else if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-		/*
-			DNS over TCP prefixes the DNS header with a 2-octet length field.
-			gopacket doesn't handle this at all...and as far as I can tell might
-			not be able to (layer parsers don't seem to be able to see anything
-			about previous layers).
+	for {
+		time.Sleep(interval)
 
-			Additionally, I've yet to find the magic LayerType that gives me a layer
-			called Payload (which is GoPacket for Data we can't parse)..so we do this
-
-			In reality we should actually care about the length field...but that will
-			come later, as we can't actually even parse the response types that are
-			likely to span multiple packets.
-		*/
-		for _, layer := range packet.Layers() {
-			if layer.LayerType().String() == "Payload" {
-				//offset the LayerContents to skip the length field
-				if dnsP := gopacket.NewPacket(layer.LayerContents()[2:], layers.LayerTypeDNS,
-					gopacket.Default); dnsP != nil {
-					return dnsP.Layers()[0].(*layers.DNS)
-				} else {
-					log.Debug("Got a non-DNS TCP packet")
-					log.Debug(packet.String())
-					return nil
-				}
+		//max_age should be negative, e.g. -1m
+		cleanupCutoff := time.Now().Add(maxAge)
+		for key, item := range *conntable {
+			if item.inserted.Before(cleanupCutoff) {
+				log.Debug("conntable GC: cleanup query ID " + strconv.Itoa(int(key)))
+				delete(*conntable, key)
 			}
 		}
-		// non-paylod TCP packets
-		return nil
 	}
-
-	log.Debug("Got a packet that is neither TCP nor UDP")
-	log.Debug(packet.String())
-
-	return nil
 }
 
-/* validate if DNS, make conntable entry and output
+/* validate if DNS packet, make conntable entry and output
    to log channel if there is a match
+   
+   we pass packet by value here because we turned on ZeroCopy for the capture, which reuses the capture buffer
 */
 func handlePacket(packets chan gopacket.Packet, logC chan dnsLogEntry,
 	gcInterval time.Duration, gcAge time.Duration) {
 
 	//DNS IDs are stored as uint16s by the gopacket DNS layer
-	//TODO: fix the memory leak of failed lookups by making this a ttlcache
 	var conntable = make(map[uint16]dnsMapEntry)
 
 	//setup garbage collection for this map
 	go cleanDnsCache(&conntable, gcAge, gcInterval)
 
-	for packet := range packets {
-		srcIP, dstIP := getIpaddrs(packet)
-		dns := getDnsLayer(packet)
+	var ethLayer layers.Ethernet
+    var ipLayer  layers.IPv4
+    var udpLayer layers.UDP
+    var tcpLayer layers.TCP
+    var dns layers.DNS
+    var payload gopacket.Payload
+	
+	//pre-allocated for initLogEntry
+	logs := []dnsLogEntry{}
+	
+	parser := gopacket.NewDecodingLayerParser(
+            layers.LayerTypeEthernet,
+            &ethLayer,
+            &ipLayer,
+            &udpLayer,
+            &tcpLayer,
+            &dns,
+            &payload,
+        )
+	
+	dnsParser := gopacket.NewDecodingLayerParser(
+            layers.LayerTypeDNS,
+            &dns,
+            &payload,
+        )
+	
+	foundLayerTypes := []gopacket.LayerType{}
 
-		if dns == nil {
+	for packet := range packets {
+		
+		parser.DecodeLayers(packet.Data(), &foundLayerTypes)
+		
+		if foundLayerType(layers.LayerTypeTCP, foundLayerTypes) && 
+			!foundLayerType(gopacket.LayerTypePayload, foundLayerTypes){
+			//TCP control packet, skip
 			continue
 		}
-
-		// Get actual DNS data from this layer
-		//dns, _ := dnsLayer.(*layers.DNS)
-
+		
+		//TODO: Actually use the legnth field and store response if it's less than the length
+		
+		//If we have a TCP packet with a payload fix our offset and re-parse
+		if foundLayerType(layers.LayerTypeTCP, foundLayerTypes) &&  
+			foundLayerType(gopacket.LayerTypePayload, foundLayerTypes){
+			//in DNS over TCP a 16 bit length field is prepended to the packet
+			//gopacket utterly fails at handling this case, so we help it out.
+			
+			//for the future int(payload[:2]) is the total legnth of the response
+			//the legnth is the length of the packet in bytes -2 (for the 2 length bytes)
+			err := dnsParser.DecodeLayers(payload[2:], &foundLayerTypes)
+			
+			if err != nil {
+        		log.Debug("Trouble decoding TCP DNS layers: ", err)
+            	log.Debug("Packet: "+packet.String())
+            	continue
+        	}
+        	
+        	if len(payload)-2 != int(binary.BigEndian.Uint16(payload[:8])) {
+        		log.Debug("multi-packet DNS response found but not yet implemented")
+        	}
+		}
+		
+		//If we have a packet that's not DNS but DOES have a payload
+		//Debug it and move on
+		if !foundLayerType(layers.LayerTypeDNS, foundLayerTypes) && 
+			foundLayerType(gopacket.LayerTypePayload, foundLayerTypes){
+			log.Debug("Got a packet with a payload but without a DNS Layer: "+packet.String())
+			continue
+		}
+		
+		srcIP := ipLayer.SrcIP 
+		dstIP := ipLayer.DstIP
+		
 		//skip non-query stuff (Updates, AXFRs, etc)
 		if dns.OpCode != layers.DNSOpCodeQuery {
 			log.Debug("Saw non-update DNS packet: " + packet.String())
@@ -329,10 +332,15 @@ func handlePacket(packets chan gopacket.Packet, logC chan dnsLogEntry,
 		if dns.QR && foundItem {
 			question := item.entry
 			//We have both legs of the connection, so drop the connection from the table
-			log.Debug("Got 'answer' leg of query ID: " + strconv.Itoa(int(question.ID)))
+			//log.Debug("Got 'answer' leg of query ID: " + strconv.Itoa(int(question.ID)))
 			delete(conntable, question.ID)
+			
+			logs = nil
+			initLogEntry(srcIP, dstIP, question, dns, &logs)
 
-			for _, logEntry := range initLogEntry(srcIP, dstIP, question, dns) {
+			//TODO: send the array itself, not the elements of the array
+			//to reduce the number of channel transactions
+			for _, logEntry := range logs {
 				logC <- logEntry
 			}
 
@@ -342,7 +350,7 @@ func handlePacket(packets chan gopacket.Packet, logC chan dnsLogEntry,
 			continue
 		} else {
 			//This is the initial query.  save it for later.
-			log.Debug("Got the 'question' leg of query ID " + strconv.Itoa(int(dns.ID)))
+			//log.Debug("Got the 'question' leg of query ID " + strconv.Itoa(int(dns.ID)))
 			mapEntry := dnsMapEntry{
 				entry:    dns,
 				inserted: time.Now(),
@@ -356,6 +364,7 @@ func handlePacket(packets chan gopacket.Packet, logC chan dnsLogEntry,
 func logConn(logC chan dnsLogEntry, quiet bool,
 	filename string, kafkaBrokers string, kafkaTopic string) {
 
+	//holds the channels for the outgoing log channels
 	var logs []chan dnsLogEntry
 
 	if !quiet {
@@ -387,6 +396,7 @@ func logConn(logC chan dnsLogEntry, quiet bool,
 	}
 }
 
+//logs to stdout
 func logConnStdout(logC chan dnsLogEntry) {
 	for message := range logC {
 		encoded, _ := message.Encode()
@@ -394,30 +404,33 @@ func logConnStdout(logC chan dnsLogEntry) {
 	}
 }
 
+//logs to a file
 func logConnFile(logC chan dnsLogEntry, filename string) {
 
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		log.Debug("could not open logfile for writing!")
+		log.Debug("could not open logfile "+filename+" for writing!")
 		panic(err)
 	}
 
 	defer f.Close()
 
+	enc := ffjson.NewEncoder(bufio.NewWriter(f))
+
 	for message := range logC {
-		encoded, _ := message.Encode()
-		f.WriteString(string(encoded) + "\n")
+		enc.Encode(message)
 	}
 }
 
+//logs to kafka
 func logConnKafka(logC chan dnsLogEntry, kafkaBrokers string, kafkaTopic string) {
 	for message := range logC {
-		//marshal to JSON.  Maybe we should do this in the log thread?
 		encoded, _ := message.Encode()
 		fmt.Println("Kafka: " + string(encoded))
 	}
 }
 
+//setup a device or pcap file for capture, returns a handle
 func initHandle(dev string, pcapFile string, bpf string) *pcap.Handle {
 
 	var handle *pcap.Handle
@@ -449,8 +462,19 @@ func initHandle(dev string, pcapFile string, bpf string) *pcap.Handle {
 	return handle
 }
 
+func foundLayerType(layer gopacket.LayerType, found []gopacket.LayerType) bool{
+	for _, value := range found {
+		if value == layer {
+			return true
+		}
+	}
+	
+	return false
+}
+
+//kick off packet procesing threads and start the packet capture loop
 func doCapture(handle *pcap.Handle, logChan chan dnsLogEntry,
-	gcAge string, gcInterval string) {
+	gcAge string, gcInterval string, numprocs int) {
 
 	gcAgeDur, err := time.ParseDuration(gcAge)
 
@@ -465,9 +489,12 @@ func doCapture(handle *pcap.Handle, logChan chan dnsLogEntry,
 	}
 
 	/* init channels for the packet handlers and kick off handler threads */
-	var channels [8]chan gopacket.Packet
-	for i := 0; i < 8; i++ {
-		channels[i] = make(chan gopacket.Packet)
+	var channels []chan gopacket.Packet
+	for i := 0; i < numprocs; i++ {
+		channels = append(channels, make(chan gopacket.Packet, 100))
+	}
+
+	for i := 0; i < numprocs; i++ {
 		go handlePacket(channels[i], logChan, gcIntervalDur, gcAgeDur)
 	}
 
@@ -475,20 +502,50 @@ func doCapture(handle *pcap.Handle, logChan chan dnsLogEntry,
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	//only decode packet in response to function calls, this moves the
 	//packet processing to the processing threads
-	packetSource.DecodeOptions = gopacket.Lazy
+	packetSource.DecodeOptions.Lazy = true
+	//We don't mutate bytes of the packets, so no need to make a copy
+	//this does mean we need to pass the packet via the channel, not a pointer to the packet
+	//as the underlying buffer will get re-allocated
+	packetSource.DecodeOptions.NoCopy = true
+
+	/*
+		parse up to the IP layer so we can consistently balance the packets across our 
+		processing threads
+		
+		TODO: in the future maybe pass this on the channel to so we don't reparse
+				but the profiling I've done doesn't point to this as a problem
+	*/
+
+	var ethLayer layers.Ethernet
+    var ipLayer  layers.IPv4
+	
+	parser := gopacket.NewDecodingLayerParser(
+            layers.LayerTypeEthernet,
+            &ethLayer,
+            &ipLayer,
+        )
+	
+	foundLayerTypes := []gopacket.LayerType{}
+
 	for packet := range packetSource.Packets() {
-		// Dispatch packets here
-		if net := packet.NetworkLayer(); net != nil {
-			/*  load balance the processiing over 8 threads
-			    FashHash is consistant for A->B and B->A hashes, which simplifies
-			    our connection tracking problem a bit by letting us keep
-			    per-worker connection pools instead of a global pool.
-			*/
-			channels[int(net.NetworkFlow().FastHash())&0x7] <- packet
+		/*  load balance the processiing over N threads
+		    FashHash is consistant for A->B and B->A hashes, which simplifies
+		    our connection tracking problem a bit by letting us keep
+		    per-worker connection pools instead of a global pool.
+		*/
+			
+		parser.DecodeLayers(packet.Data(), &foundLayerTypes)
+		
+		if foundLayerType(layers.LayerTypeIPv4, foundLayerTypes) {
+			channels[int(ipLayer.NetworkFlow().FastHash()) & (numprocs-1)] <- packet
+		}else{
+    		log.Debug("Saw a non-IPv4 Packet: "+packet.String())
 		}
+    	
 	}
 }
 
+//setup logging settings
 func initLogging(debug bool) chan dnsLogEntry {
 	if debug {
 		log.SetLevel(log.DebugLevel)
@@ -497,7 +554,7 @@ func initLogging(debug bool) chan dnsLogEntry {
 	//TODO: further logging setup?
 
 	/* spin up logging channel */
-	var logChan = make(chan dnsLogEntry)
+	var logChan = make(chan dnsLogEntry, 100)
 
 	return logChan
 
@@ -515,8 +572,19 @@ func main() {
 	var gcAge = flag.String("gc_age", "-1m", "How old a connection table entry should be before it is garbage collected.")
 	var gcInterval = flag.String("gc_interval", "3m", "How often to run garbage collection.")
 	var debug = flag.Bool("debug", false, "Enable debug logging")
+	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	var numprocs = flag.Int("numprocs", 8, "number of packet processing threads")
 
 	flag.Parse()
+
+	if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+    }
 
 	handle := initHandle(*dev, *pcapFile, *bpf)
 
@@ -530,6 +598,6 @@ func main() {
 	go logConn(logChan, *quiet, *logFile, *kafkaBrokers, *kafkaTopic)
 
 	//spin up the actual capture threads
-	doCapture(handle, logChan, *gcAge, *gcInterval)
+	doCapture(handle, logChan, *gcAge, *gcInterval, *numprocs)
 
 }
